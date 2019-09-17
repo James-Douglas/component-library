a4701a57fe61120b5908c41a487a86fa9541457c
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-09-17 16:06:17 +1000

    config for manor-react

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import { create } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: create({
+      base: 'light',
+      brandTitle: 'CTM Manor-react'
+    }),
+    isFullscreen: false,
+    panelPosition: 'right',
+    showPanel: true,
+  }
+});
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module);
