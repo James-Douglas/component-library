4c4c28fb4b08668be52bc52b1e9845cf4113031f
Matthew Shine <mshine@c00284.budgetdirect.com.au>
2019-09-18 10:12:14 +1000

    feat(config): cleanup boilerplate, add husky, commitizen, testing-lib

---
+++ src/components/Input/Input.component.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../index.css';
+
+class ManorInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    const { content } = this.props;
+    const { value } = this.state;
+    return (
+      <>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label>
+          {content}
+          <input type="text" value={value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </>
+    );
+  }
+}
+
+ManorInput.propTypes = {
+  content: PropTypes.string,
+};
+
+ManorInput.defaultProps = {
+  content: '',
+};
+
+export default ManorInput;
