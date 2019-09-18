bcb8419e489e61bdda96e3e19f5952298a526841
nik kyriakidis <nikolaas.kyriakidis@comparethemarket.com.au>
2019-09-18 11:24:20 +1000

    saving changes

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,55 @@
+
+const glob = require('glob');
 const { POSTCSS_MODES } = require("@craco/craco");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+const gatherComponents = () => {
+    const entryPoints = {};
+
+    const files = glob.sync(__dirname + "/src/components/**/*.component.js")
+    console.log(files)
+    files.map((e) => {
+        const key = 
+    })
+}
+
+const filterPlugins = (webpackConfig, pluginType) => {
+    const i = webpackConfig.plugins.findIndex((e) => typeof e == pluginType)
+    return webpackConfig.plugins.splice(i, 1)
+}
 
 module.exports = {
     style: {
         postcss: {
             mode: POSTCSS_MODES.file
+        }
+    },
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            gatherComponents()
+            paths.appBuild = __dirname + "/lib"
+            webpackConfig.entry = __dirname + '/src/components/index.js';
+            webpackConfig.output = {
+                path: __dirname + '/lib',
+                library: "",
+                libraryTarget: 'commonjs-module'
+            };
+            webpackConfig.plugins = filterPlugins(webpackConfig, HtmlWebpackPlugin);
+            const { GenerateSW } = WorkboxWebpackPlugin;
+            webpackConfig.plugins = filterPlugins(webpackConfig, GenerateSW);
+            webpackConfig.plugins = filterPlugins(webpackConfig, InterpolateHtmlPlugin);
+            webpackConfig.plugins = filterPlugins(webpackConfig, ManifestPlugin);
+            webpackConfig.plugins.push(new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: '[name].css',
+            }))
+            webpackConfig.optimization.runtimeChunk = false;
+            return webpackConfig;
         }
     }
 };
