79ca4f765b2a89c4e4e0ac83a68967ae6be1a724
Jason McKechnie <jason.mckechnie@comparethemarket.com.au>
2019-09-19 14:15:51 +1000

    feat(textarea): build textarea - wip - functional component

--- src/components/Textarea/Textarea.component.js
+++ src/components/Textarea/Textarea.component.js
@@ -1,131 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Textarea.module.css';
 
-class ManorTextarea extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: props.value };
-    this.handleChange = this.handleChange.bind(this);
-    this.isDirty = false;
-    this.charsExceed = false;
-    if (props.maxLength || props.maxChars) {
-      this.charLimit = props.maxChars ? props.maxChars : props.maxLength;
-      this.textareaRemainChars = props.value ? this.charLimit - props.value.length : this.charLimit;
-    }
+const ManorTextarea = ({
+  id,
+  name,
+  label,
+  placeholder,
+  value,
+  bordered,
+  disabled,
+  required,
+  invalid,
+  autofill,
+  hidden,
+  rows,
+  wrap,
+  readonly,
+  maxChars,
+  maxLength,
+}) => {
+  let isDirty = false;
+  let charsExceed = false;
+  let charLimit;
+  let textareaRemainChars;
+  let optionalField;
+  let maxlengthIndicator;
+
+
+  if (maxLength || maxChars) {
+    charLimit = maxChars || maxLength;
+    textareaRemainChars = value ? charLimit - value.length : charLimit;
   }
 
-  handleChange(event) {
-    this.isDirty = true;
+  const [stateValue, setStateValue] = useState(value);
 
-    const {
-      maxChars,
-      maxLength,
-    } = this.props;
-
+  const handleChange = (event) => {
+    isDirty = true;
+    console.log("hello");
     if (maxLength || maxChars) {
-      this.charLimit = maxChars || maxLength;
-
-      /* eslint-disable-next-line max-len */
-      this.textareaRemainChars = event.target.value ? this.charLimit - event.target.value.length : this.charLimit;
-
-      if (this.textareaRemainChars < 0) {
+      charLimit = maxChars || maxLength;
+      textareaRemainChars = event.target.value ? charLimit - event.target.value.length : charLimit;
+      console.log(textareaRemainChars);
+      if (textareaRemainChars < 0) {
         event.target.setCustomValidity('Maximum Characters exceeded');
-        this.charsExceed = true;
+        charsExceed = true;
       } else {
-        if (this.charsExceed) {
+        if (charsExceed) {
           event.target.setCustomValidity('');
         }
-        this.charsExceed = false;
+        charsExceed = false;
       }
-    }
-
-    this.setState({ value: event.target.value });
-  }
-
-  render() {
-    let optionalField;
-    let maxlengthIndicator;
-    const { value } = this.state;
-
-    const {
-      id,
-      name,
-      label,
-      placeholder,
-      bordered,
-      disabled,
-      required,
-      invalid,
-      autofill,
-      hidden,
-      rows,
-      wrap,
-      readonly,
-      maxChars,
-      maxLength,
-    } = this.props;
-
-    if (!required) {
-      optionalField = (
-        <span className={`${styles['manor-optional-indicator']} manor-subscript`} id={`${id}-optional-indicator`}>
-          <span className="sr-only">
-            {`The ${label} field is `}
-          </span>
-          Optional
-        </span>
-      );
-    }
-
-    if (maxLength || maxChars) {
       maxlengthIndicator = (
-        <span id={`${id}-maxlength-indicator`} className={`manor-maxlength-indicator manor-subscript ${this.textareaRemainChars < 0 ? `${styles['max-chars-exceeded']}` : ''} `}>
-          <span className="sr-only">
-            {`${this.textareaRemainChars && this.textareaRemainChars < 0 ? ' Exceeded character limit' : ' Remaining allowed characters'} for the ${label} field `}
-          </span>
-          {this.textareaRemainChars}
+          <span id={`${id}-maxlength-indicator`} className={`manor-maxlength-indicator manor-subscript ${textareaRemainChars < 0 ? `${styles['max-chars-exceeded']}` : ''} `}>
+        <span className="sr-only">
+          {`${textareaRemainChars && textareaRemainChars < 0 ? ' Exceeded character limit' : ' Remaining allowed characters'} for the ${label} field `}
         </span>
+            {textareaRemainChars}
+      </span>
       );
     }
+    console.log("hello there");
+    setStateValue(event.target.value);
+  };
 
-    return (
-      <>
-        <div className={`${styles['manor-textarea-wrapper']} ${disabled ? `${styles.disabled}` : ''} ${hidden ? `${styles.hidden}` : ''} ${!bordered ? `${styles['borderless-field']}` : ''} `}>
-          <label htmlFor={id}>
-
-            {label && label.length > 0
-              && <div className={`manor-textarea-label ${styles['label-default']} manor-body1 manor-spacing-label-to-field ${disabled && bordered ? `${styles['manor-bordered-field-disabled-label']}` : ''} `}>{`${label}`}</div>}
-
-            <div className={`${styles['pull-tab']} ${(autofill && !this.isDirty) ? `${styles['manor-prefilled']}` : ''} ${disabled ? `${styles['manor-disabled']}` : ''} `} />
-
-            <textarea
-              value={value}
-              onChange={this.handleChange}
-              id={id}
-              name={name}
-              placeholder={placeholder}
-              disabled={disabled}
-              required={required}
-              rows={rows}
-              wrap={wrap}
-              readOnly={readonly}
-              maxLength={maxLength}
-              className={`${styles['manor-textarea-default']} manor-body2 ${bordered ? `${styles['manor-textarea-border']}` : ''}  ${(autofill && !this.isDirty) ? `${styles['manor-prefilled']}` : ''}  ${invalid || (this.textareaRemainChars < 0) ? `${styles.invalid}` : ''} `}
-              aria-describedby={`${!required ? `${id}-optional-indicator` : ''} ${maxLength || maxChars ? `${id}-maxlength-indicator` : ''}  `}
-            />
-          </label>
-
-          <div className={`${styles['supporting-elements']}`}>
-            {maxlengthIndicator}
-            {' '}
-            {optionalField}
-          </div>
-
-        </div>
-      </>
+  if (!required) {
+    optionalField = (
+      <span className={`${styles['manor-optional-indicator']} manor-subscript`} id={`${id}-optional-indicator`}>
+        <span className="sr-only">
+          {`The ${label} field is `}
+        </span>
+        Optional
+      </span>
     );
   }
-}
+
+  if (maxLength || maxChars) {
+    maxlengthIndicator = (
+      <span id={`${id}-maxlength-indicator`} className={`manor-maxlength-indicator manor-subscript ${textareaRemainChars < 0 ? `${styles['max-chars-exceeded']}` : ''} `}>
+        <span className="sr-only">
+          {`${textareaRemainChars && textareaRemainChars < 0 ? ' Exceeded character limit' : ' Remaining allowed characters'} for the ${label} field `}
+        </span>
+        {textareaRemainChars}
+      </span>
+    );
+  }
+
+  return (
+    <>
+      <div className={`${styles['manor-textarea-wrapper']} ${disabled ? `${styles.disabled}` : ''} ${hidden ? `${styles.hidden}` : ''} ${!bordered ? `${styles['borderless-field']}` : ''} `}>
+        <label htmlFor={id}>
+
+          {label.length > 0
+            && <div className={`manor-textarea-label ${styles['label-default']} manor-body1 manor-spacing-label-to-field ${disabled && bordered ? `${styles['manor-bordered-field-disabled-label']}` : ''} `}>{`${label}`}</div>}
+
+          <div className={`${styles['pull-tab']} ${(autofill && !isDirty) ? `${styles['manor-prefilled']}` : ''} ${disabled ? `${styles['manor-disabled']}` : ''} `} />
+
+          <textarea
+            value={stateValue}
+            onChange={handleChange}
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            disabled={disabled}
+            required={required}
+            rows={rows}
+            wrap={wrap}
+            readOnly={readonly}
+            maxLength={maxLength}
+            className={`${styles['manor-textarea-default']} manor-body2 ${bordered ? `${styles['manor-textarea-border']}` : ''}  ${(autofill && !isDirty) ? `${styles['manor-prefilled']}` : ''}  ${invalid || (textareaRemainChars < 0) ? `${styles.invalid}` : ''} `}
+            aria-describedby={`${!required ? `${id}-optional-indicator` : ''} ${maxLength || maxChars ? `${id}-maxlength-indicator` : ''}  `}
+          />
+        </label>
+
+        <div className={`${styles['supporting-elements']}`}>
+          {maxlengthIndicator}
+          {' '}
+          {optionalField}
+        </div>
+
+      </div>
+    </>
+  );
+};
 
 ManorTextarea.propTypes = {
   id: PropTypes.string.isRequired,
