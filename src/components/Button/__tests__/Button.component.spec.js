130afce2dcbb91d1c7e7a56a09386777700cc572
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-09-23 08:59:32 +1000

    feat(buttons): unit tests

--- src/components/Button/__tests__/Button.component.spec.js
+++ src/components/Button/__tests__/Button.component.spec.js
@@ -4,14 +4,13 @@ import Button from '../Button.component';
 
 /* id, btnType, btnMode, btnSize, content, disabled, icon, size, iconAlignRight, href, target, */
 describe('Button', () => {
-
   it('renders correctly with minimal props', () => {
-    const { container } = render(<Button id={'test-id'}/>);
+    const { container } = render(<Button id="test-id" />);
     expect(container).toMatchSnapshot();
   });
 
   it('renders with props', () => {
-    const { container, getByText } = render(<Button id={'test-id'} btnType={'primary'} btnMode={'onDark'} btnSize={'md'} content={'test content'} disabled={true} icon={'check'} iconAlignRight={true}/>);
+    const { container, getByText } = render(<Button id="test-id" btnType="primary" btnMode="onDark" btnSize="md" content="test content" disabled icon="check" iconAlignRight />);
 
     const btn = container.querySelector('#test-id');
     const icon = btn.querySelector('.btn-icon');
@@ -27,7 +26,7 @@ describe('Button', () => {
   });
 
   it('renders a primary button when the prop is supplied', () => {
-    const { container } = render(<Button id={'test-id'} btnType={'primary'} />);
+    const { container } = render(<Button id="test-id" btnType="primary" content='test thing' />);
 
     const btn = container.querySelector('#test-id');
     const btnTypeAttr = btn.getAttribute('btntype');
@@ -36,7 +35,7 @@ describe('Button', () => {
   });
 
   it('renders a secondary button in mode = onDark, when the props are supplied', () => {
-    const { container } = render(<Button id={'test-id'} btnType={'secondary'} btnMode={'onDark'} />);
+    const { container } = render(<Button id="test-id" btnType="secondary" btnMode="onDark" content='test thing' />);
 
     const btn = container.querySelector('#test-id');
     const btnTypeAttr = btn.getAttribute('btntype');
@@ -44,5 +43,39 @@ describe('Button', () => {
 
     expect(btnTypeAttr).toBe('secondary');
     expect(btnModeAttr).toBe('onDark');
+  });
+
+  it('renders a text button with an icon when specified', () => {
+    const { container } = render(<Button id="test-id" btnType="text" icon="check" content='test thing' />);
+
+    const btn = container.querySelector('#test-id');
+    const btnTypeAttr = btn.getAttribute('btntype');
+    const icon = container.querySelector('.btn-icon').classList[0];
+    
+    expect(btnTypeAttr).toBe('text');
+    expect(btn.getAttribute('btnMode')).toBe('');
+    expect(icon).toBe('btn-icon');
+  });
+
+  it('renders a link button (a href) when specified', () => {
+    const { container } = render(<Button id="test-id" btnType="link" href='#' content='test thing' />);
+
+    const btn = container.querySelector('#test-id');
+    const btnTypeAttr = btn.getAttribute('btntype');
+
+    expect(btnTypeAttr).toBe('link');
+    expect(btn.getAttribute('href')).toBe('#');
+    expect(btn.getAttribute('btnMode')).toBe('');
+  });
+
+  it('renders a footer link (a href) when specified', () => {
+    const { container } = render(<Button id="test-id" btnType="footer-link" href='#' content='test thing' />);
+
+    const btn = container.querySelector('#test-id');
+    const btnTypeAttr = btn.getAttribute('btntype');
+
+    expect(btnTypeAttr).toBe('footer-link');
+    expect(btn.getAttribute('href')).toBe('#');
+    expect(btn.getAttribute('btnMode')).toBe('');
   });
 });
