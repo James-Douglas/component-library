ee949e41181f2d4a5096a8c773156aab9a063eae
Iuliia Ostapenko <iuliia.ostapenko@comparethemarket.com.au>
2019-10-02 07:53:14 +1000

    feat(hooks test): hook test

---
+++ src/hooks/__tests__/useIsSticky.spec.js
@@ -0,0 +1,40 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useIsSticky from '../useIsSticky';
+
+describe('useIsSticky', () => {
+  it('should stick if scroll more than initial offset', () => {
+    const initialOffset = 10;
+    const scrollTop = 20;
+    // define event mapping
+    const map = {};
+    window.addEventListener = jest.fn((event, cb) => {
+      map[event] = cb;
+    });
+
+    const { result } = renderHook(() => useIsSticky(initialOffset));
+    // set scroll
+    global.window.scrollY = scrollTop;
+    // trigger scroll event
+    map.scroll();
+
+    expect(result.current).toBe(true);
+  });
+
+  it('should stick if scroll more than initial offset', () => {
+    const initialOffset = 10;
+    const scrollTop = 5;
+    // define event mapping
+    const map = {};
+    window.addEventListener = jest.fn((event, cb) => {
+      map[event] = cb;
+    });
+
+    const { result } = renderHook(() => useIsSticky(initialOffset));
+    // set scroll
+    global.window.scrollY = scrollTop;
+    // trigger scroll event
+    map.scroll();
+
+    expect(result.current).toBe(false);
+  });
+});
