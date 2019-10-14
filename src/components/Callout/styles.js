a9d9513fc0fe7839de63b9b03ade6956dbc62700
Iuliia Ostapenko <iuliia.ostapenko@comparethemarket.com.au>
2019-10-14 11:05:14 +1000

    feat(callout): united things

--- src/components/Callout/styles.js
+++ src/components/Callout/styles.js
@@ -2,8 +2,7 @@ import css from 'styled-jsx/css';
 
 export default css`
 .callout {
-  @apply text-base font-semibold leading-tight;
-  @apply border-l-4 border-dark-blue bg-white py-12 px-20 text-base font-normal leading-tight;
+  @apply text-base font-semibold leading-tight border-l-4 border-dark-blue bg-white py-12 px-20 text-base font-normal leading-tight;
 }
 .bg-grey {
  @apply bg-grey-lighter;
