864d3358b98ab07295ee2876323304521d44dc11
Iuliia Ostapenko <iuliia.ostapenko@comparethemarket.com.au>
2019-10-14 13:44:44 +1000

    fix(accordion): padding changes

--- src/components/Accordion/styles.js
+++ src/components/Accordion/styles.js
@@ -2,39 +2,39 @@ import css from 'styled-jsx/css';
 
 export default css`
 .accordion {
+  transition: color 2s linear;
   border-bottom: 2px solid #999999;
+  overflow: hidden;
 }
 .accordion .accordion-body {
-    transition: font-size .15s, margin .15s, padding .15s,
-                opacity .5s .15s;
-
+  @apply pt-16 pl-32 pb-24 pr-48 bg-white text-xl leading-normal text-xl;
+  color: rgba(0,0,0,0.8);
+  display: block;
 }
-
+.accordion.hide .accordion-body {
+  display: none;
+}
 
 .accordion.hide {
    border-bottom: 1px solid rgba(0,0,0,.1);
 }
-.accordion.hide .accordion-body {
-    font-size: 0;
-    margin: 0;
-    opacity: 0;
-    padding: 0;
-    max-height: 0;
-    /* fade out, then shrink */
-    transition: opacity .15s, font-size .5s .25s, margin .5s .15s, padding .5s .15s, max-height .5s .15s;
-}
-.accordion-body-wrap {
-  margin: 15px 25px 15px 15px; 
+.accordion > * {
+  @apply bg-white;
 }
 .accordion-head {
-padding: 15px 25px 15px 15px; 
-} 
-.accordion-head {
-  @apply flex justify-between items-center;
-  @apply font-bold text-base;
-  @apply bg-white cursor-pointer;
+  @apply pt-24 pl-32 pb-24 pr-48 flex justify-between items-center text-base bg-white cursor-pointer text-2xl;
+  transition: font-size 0.5s; 
+  overflow: hidden;
+  z-index:2;
+  position: relative;
+}
+.accordion.hide .accordion-head  {
+   @apply text-lg;
 }
 .accordion-body {
    @apply text-sm;
+}
+*:focus {
+  outline: none;
 }
 `;
