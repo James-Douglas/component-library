defa077d9f1c4db671e96ef5cb73e63bd46df4f8
Daniel Wright <daniel.wright@comparethemarket.com.au>
2019-10-31 08:57:35 +1000

    bugfix/DO-745/fix-release-process (#84)
    * chore(ci): resolve issues with release tooling and set correct npmrc
    
    * chore(ci): update readme

--- deployment/bamboo/release.sh
+++ deployment/bamboo/release.sh
@@ -2,62 +2,70 @@
 set -e
 source ~/.nvm/nvm.sh
 
-nvm install v10.16.3
-nvm use v10.16.3
+echo "running: nvm install v10.17.0"
+nvm install v10.17.0
 if [ "$?" -ne 0 ]; then
-  echo "ERROR - Could not set nvm version specified in .nvmrc"
+  echo "ERROR - Could not install the correct node version with nvm"
   exit 1
 fi
 
+echo "running: nvm use v10.17.0"
+nvm use v10.17.0
+if [ "$?" -ne 0 ]; then
+  echo "ERROR - Could not set the correct node version with nvm"
+  exit 1
+fi
+
+echo "running: git remote set-url origin https://github.com/comparethemarketau/manor-react.git"
 git remote set-url origin https://github.com/comparethemarketau/manor-react.git
 if [ "$?" -ne 0 ]; then
   echo "ERROR - Could not set git remote"
   exit 1
 fi
 
+echo "running: git fetch origin"
 git fetch origin
 if [ "$?" -ne 0 ]; then
   echo "ERROR - Could not fetch git"
   exit 1
 fi
 
+echo "running: git tag -l"
 git tag -l
 if [ "$?" -ne 0 ]; then
   echo "ERROR - Could not fetch git"
   exit 1
 fi
 
+echo "running: yarn"
 yarn
 if [ "$?" -ne 0 ]; then
   echo "ERROR - yarn failed"
   exit 1
 fi
 
+echo "running: yarn test:ci"
 yarn test:ci
 if [ "$?" -ne 0 ]; then
   echo "ERROR - tests failed"
   exit 1
 fi
 
+echo "running: yarn lint"
 yarn lint
 if [ "$?" -ne 0 ]; then
   echo "ERROR - linting failed"
   exit 1
 fi
 
+echo "running: yarn build"
 yarn build
 if [ "$?" -ne 0 ]; then
   echo "ERROR - yarn build failed"
   exit 1
 fi
 
-# switch node versions so we can deploy
-nvm use 8.16.0
-if [ "$?" -ne 0 ]; then
-  echo "ERROR - Could not set nvm version to 8.16.0"
-  exit 1
-fi
-
+echo "running: npx semantic-release"
 npx semantic-release
 if [ "$?" -ne 0 ]; then
   echo "ERROR - release failed"
