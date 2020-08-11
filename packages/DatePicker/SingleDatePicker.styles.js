5832e129e30fcd9774e6772f7f0d872a6d42c445
Matthew Klemm <matthew_klemm@live.com>
2020-08-12 09:36:35 +1000

    Feature/gp 379 update date picker (#358)
    * feat(datepicker): fixed datepicker validation; allow full width
    
    * feat(datepicker): added isDayBlocked validation errors
    
    * style(datepicker): removed right margin from SingleDatePicker

--- packages/DatePicker/SingleDatePicker.styles.js
+++ packages/DatePicker/SingleDatePicker.styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const StyledDateRangePickerWrap = styled.div`
-  margin-right: ${({ theme }) => theme.spacing[24]};
   .date-input-calendar {
     padding-right: ${({ theme }) => theme.spacing[12]};
   }
